da0f11f66ca87793c63fadfa6368f19e5b27d40c
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-22 21:13:35 +0530

    Data Fetching

--- Data Fetching/index.js
+++ Data Fetching/index.js
@@ -1,2 +1,9 @@
 // console.log("fetching data from api");
 
+const fetchData = async () =>{
+    let api = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    let data = await api.json();
+    console.log(data)
+}
+
+fetchData();
