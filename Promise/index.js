ce82606ecc0885c2ce8c39e68914a34a09b35446
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-22 12:33:34 +0530

    Promise

--- Promise/index.js
+++ Promise/index.js
@@ -7,3 +7,20 @@
 // Fullfiled : Opertion succeeded, giving a resolved value
 
 // Rejected : Opertion failed, providing a reason (error) 
+
+let doHomework = new Promise((resolve, reject)=> {
+    let completed = true; //Change to false to test rejection
+
+    console.log("Starting the My homework");
+
+    setTimeout(()=>{
+        if(completed){
+            resolve:({msg:"Home work Finished" , value:20})
+        }else{
+            reject(`Did'nt finish homework`)
+        }
+    },2000); //2 sec only
+
+})
+
+doHomework.then(msg =>console.log(msg.value)).catch(err=> console.log(err))
