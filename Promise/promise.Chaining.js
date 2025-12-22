2862423e0ca4132aca8c13a273386362b00cd4ca
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-22 12:53:31 +0530

    Promise & promise.chaining

---
+++ Promise/promise.Chaining.js
@@ -0,0 +1,35 @@
+const doHomework = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Homework done✅")
+        }, 2000); // 2 sec only
+    })
+}
+
+const revision = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Revision done✅")
+        }, 1500); //1.5 sec only
+    })
+}
+
+const Games = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Played game done✅")
+        }, 1000); //1 sec only
+    })
+}
+
+doHomework().then(res =>{
+    console.log(res);
+    return revision();
+}).then(res => {
+    console.log(res);
+    return Games();
+}).then(res => {
+    console.log(res);
+    // return Games();
+})
+.catch(err => console.log(err))
