32889312f2619c56a4530126d8184a95d2abdc45
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-23 07:28:50 +0530

    Loop

--- Loop/index.js
+++ Loop/index.js
@@ -5,10 +5,15 @@ type of loops-
 1. for loop - number of iteration that we known
 
 syntax :  
-for (let i = 1; i <= 10; i++){
+for (let i = 1; i <= 5; i++){
     console.log("Aryan",i);
 }
 
-
+output: 
+Aryan 1
+Aryan 2
+Aryan 3
+Aryan 4
+Aryan 5
 */
 
