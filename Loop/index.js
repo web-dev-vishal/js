0dd479717f98f758cc1288a36391eeab913c279f
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-23 07:37:28 +0530

    Loop

--- Loop/index.js
+++ Loop/index.js
@@ -1,9 +1,11 @@
-console.log("We are in a loop")
+// console.log("We are in a loop")
 
 /*
 type of loops-
 1. for loop - number of iteration that we known
+2. while loop - 
 
+for loop
 syntax :  
 for (let i = 1; i <= 5; i++){
     console.log("Aryan",i);
@@ -15,5 +17,25 @@ Aryan 2
 Aryan 3
 Aryan 4
 Aryan 5
+
+ while loop
+
+syntax:
+let i = 1;
+
+ while(i<= 5){
+     console.log("Aryan", i);
+     i++
+ }
+ 
+output: 
+Aryan 1
+Aryan 2
+Aryan 3
+Aryan 4
+Aryan 5
+
 */
+
+let 
 
