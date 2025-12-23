00a4286b4cc47838eac678350f1dc3d92f4f9c76
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-23 07:27:46 +0530

    Loop

--- Loop/index.js
+++ Loop/index.js
@@ -1,3 +1,14 @@
 console.log("We are in a loop")
 
-//type of loops-
+/*
+type of loops-
+1. for loop - number of iteration that we known
+
+syntax :  
+for (let i = 1; i <= 10; i++){
+    console.log("Aryan",i);
+}
+
+
+*/
+
