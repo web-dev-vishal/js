bab9001b01808db372ab2086eb3107ce94b86fa9
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-23 08:00:00 +0530

    update in sync & Async

--- Sync & Async/index.js
+++ Sync & Async/index.js
@@ -1,13 +1,44 @@
-// synchronnus
-console.log('Start');
-console.log('This will run');
-console.log('End');
+// <------------------ Synchronous ---------------------->
+// Synchronous - code runs line by line, one after another
 
-// <--------------------------------->
-// asynchronnus
-console.log('Aryan');
+console.log('Start');           // runs first
+console.log('This will run');   // runs second
+console.log('End');             // runs third
 
-setTimeout(()=>{
-    console.log('Yash');
-})
-console.log('Vedant');
+/*
+output:
+Start
+This will run
+End
+*/
+
+
+// <------------------ Asynchronous ---------------------->
+// Asynchronous - some code runs later, doesn't wait in line
+
+console.log('Aryan');           // runs first
+
+setTimeout(() => {              // this waits and runs last (after 0ms delay)
+    console.log('Sagar');       
+}, 0)
+
+console.log('Vedant');          // runs second
+
+/*
+output:
+Aryan
+Vedant
+Sagar
+*/
+
+/*
+Why this happens:
+- console.log('Aryan') runs immediately
+- setTimeout puts 'Sagar' in waiting (even with 0ms delay)
+- console.log('Vedant') runs immediately
+- 'Sagar' runs after everything else finishes
+
+Summary:
+- Synchronous: runs in order, top to bottom
+- Asynchronous: some code waits and runs later
+*/
